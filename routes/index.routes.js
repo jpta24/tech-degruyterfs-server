bb6fc0b87417b7c6d97415f8f481da700ab054c9
Jpta24 <jeanpierretorres@gmail.com>
2023-03-19 20:44:39 +0100

    search done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,40 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
+const Book = require('../models/Book.model');
+
+router.get('/books/search/:searchText', (req, res, next) => {
+	const searchText = req.params.searchText;
+
+	Book.find({
+		$or: [
+			{ title: { $regex: searchText, $options: 'i' } },
+			{ isbn: { $regex: searchText, $options: 'i' } },
+		],
+	})
+		.sort({ title: 'asc' })
+		.select('title isbn')
+		.then((books) => {
+			res.status(200).json(books);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({ message: 'Sorry internal error occurred' });
+		});
+});
+
+router.get('/test', (req, res, next) => {
+
+    Book.find()
+    .then(books=>{
+        let duplicates = [];
+        books.map(book=>book.isbn)
+        .forEach((item, index) => {
+          if (books.map(book=>book.isbn).indexOf(item, index + 1) !== -1 && duplicates.indexOf(item) === -1) {
+            duplicates.push(item);
+          }
+        })
+        res.json(duplicates);
+    })	
 });
 
 module.exports = router;
